a3921fb2610772f9193360df924f490b4a24c4d6
Gordon.Gan <571661991@qq.com>
2021-02-23 19:49:41 +0800

    test

--- house-biz/src/main/java/com/mooc/house/biz/components/FileComponent.java
+++ house-biz/src/main/java/com/mooc/house/biz/components/FileComponent.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Component
 public class FileComponent {
     @Value("${file.path:}")
-    private String filePath;
+        private String filePath;
 
     public List<String> getImgPaths(List<MultipartFile> files) {
         if (Strings.isNullOrEmpty(filePath)) {
